047f634abbe560be5e0f0b42581b1a8231ed25c3
Sakar Khadka <khadka.sakar10@gmail.com>
2025-03-01 02:59:01 +0545

    responsive navbar

---
+++ src/Components/NavBar.tsx
@@ -0,0 +1,68 @@
+"use client";
+import Image from "next/image";
+import React, { useState } from "react";
+import { FaBarsStaggered, FaXmark } from "react-icons/fa6";
+
+const NavBar = () => {
+  const navLinks = ["Home", "About", "Blogs", "Contact Us"];
+  const [open, setOpen] = useState(false);
+  return (
+    <div className="px-6 md:px-14 py-6">
+      <div className=" flex justify-between items-center">
+        <Image
+          src="/Logo/logo.webp"
+          alt="Logo"
+          width={150}
+          height={100}
+          priority={true}
+          className="w-40 md:52 h-auto"
+        />
+        <div>
+          <div className="hidden md:block">
+            {navLinks.map((link, index) => {
+              return (
+                <span
+                  key={index}
+                  className="ml-10 cursor-pointer hover:text-amber-300"
+                >
+                  {link}
+                </span>
+              );
+            })}
+          </div>
+
+          {/* THis is for responsive Icon */}
+          <div className=" text-4xl md:hidden">
+            {open ? (
+              <button onClick={() => setOpen(false)} aria-label="Close menu">
+                <FaXmark />
+              </button>
+            ) : (
+              <button onClick={() => setOpen(true)} aria-label="Open menu">
+                <FaBarsStaggered />
+              </button>
+            )}
+          </div>
+
+          {/* This is For Mobile Responsive Icons */}
+          {open && (
+            <div className="absolute left-0 top-0 pt-6 h-full bg-gray-800 w-2/3 ">
+              {navLinks.map((link, index) => {
+                return (
+                  <div
+                    key={index}
+                    className="text-3xl text-white text-center leading-16"
+                  >
+                    <a href="#">{link}</a>
+                  </div>
+                );
+              })}
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
